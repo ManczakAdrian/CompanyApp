0aeac9dd524447cd62204f74d0b316dff1112872
Adrian Manczak <manczakadrian@gmail.com>
2024-01-02 04:06:20 +0100

    controllers_update

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,89 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-//const db = require('./../db');
 
-const ObjectId = require('mongodb').ObjectId;
+const EmployeeController = require("../controllers/employees.controller");
 
-router.get('/employees', (req, res) => {
-  req.db.collection('employees')
-    .find()
-    .toArray()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-  //res.json(db.employees);
-});
-
-router.get('/employees/random', (req, res) => {
-  req.db.collection('employees')
-    .aggregate([{ $sample: { size: 1 } }])
-    .toArray()
-    .then((data) => {
-      res.json(data[0]);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-  //res.json(db.employees[Math.floor(Math.random() * db.length)]);
-});
-
-router.get('/employees/:id', (req, res) => {
-  req.db.collection('employees')
-  .findOne({ _id: ObjectId(req.params.id) })
-  .then((data) => {
-    if(!data) res.status(404).json({ message: 'Not found' });
-    else res.json(data);
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  });
-  //res.json(db.employees.find(item => item.id == req.params.id));
-});
-
-router.post('/employees', (req, res) => {
-  const { firstName, lastName,department  } = req.body;
-  req.db.collection('employees')
-  .insertOne({ firstName: firstName,lastName: lastName, department:department })
-  
-  .then(() => {
-    res.json({ message: 'OK' });
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  })
-  //db.employees.push({ id: 3, firstName, lastName })
-  //res.json({ message: 'OK' });
-});
-
-router.put('/employees/:id', (req, res) => {
-  const { firstName, lastName,department  } = req.body;
-  req.db.collection('employees')
-  .updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName: firstName,lastName: lastName, department:department  }})
-  .then(() => {
-    res.json({ message: 'OK' });
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  })
-  //db = db.employees.map(item => (item.id == req.params.id) ? { ...item, firstName, lastName } : item );
- // res.json({ message: 'OK' });
-});
-
-router.delete('/employees/:id', (req, res) => {
-  //const { firstName, lastName } = req.body;
-  req.db.collection('employees')
-  .deleteOne({ _id: ObjectId(req.params.id) })
-  .then(() => {
-    res.json({ message: 'OK' });
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  })
-  //db = db.employees.filter(item => item.id != req.params.id)
-  //res.json({ message: 'OK' });
-});
+router.get("/employees", EmployeeController.getAll);
+router.get("/employees/random", EmployeeController.getRandom);
+router.get("/employees/:id", EmployeeController.getById);
+router.post("/employees", EmployeeController.createEmployee);
+router.put("/employees/:id", EmployeeController.updateEmployee);
+router.delete("/employees/:id", EmployeeController.deleteEmployee);
 
 module.exports = router;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,93 +1,13 @@
-// post.routes.js
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('./../db');
 
-const ObjectId = require('mongodb').ObjectId;
+const ProductController = require("../controllers/products.controller");
 
-router.get('/products', (req, res) => {
-  req.db.collection('products')
-    .find()
-    .toArray()
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-  //res.json(db.products);
-});
-
-router.get('/products/random', (req, res) => {
-  req.db.collection('products')
-    .aggregate([{ $sample: { size: 1 } }])
-    .toArray()
-    .then((data) => {
-      res.json(data[0]);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-
- //res.json(db.products[Math.floor(Math.random() * db.length)]);
-});
-
-router.get('/products/:id', (req, res) => {
-  req.db.collection('products')
-  .findOne({ _id: ObjectId(req.params.id) })
-  .then((data) => {
-    if(!data) res.status(404).json({ message: 'Not found' });
-    else res.json(data);
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  });
- // res.json(db.products.find(item => item.id == req.params.id));
-});
-
-router.post('/products', (req, res) => {
-  const { name, client } = req.body;
-  req.db.collection('products')
-  .insertOne({ name: name, client:client })
-  .then(() => {
-    res.json({ message: 'OK' });
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  })
-  //db.products.push({ id: 3, name, client })
-  //res.json({ message: 'OK' });
-});
-
-router.put('/products/:id', (req, res) => {
-  const { name, client } = req.body;
-  req.db.collection('products')
-  .updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name, client:client }})
-  .then(() => {
-    res.json({ message: 'OK' });
-  })
-  .catch((err) => {
-    res.status(500).json({ message: err });
-  })
-  
-  //db = db.products.map(item => (item.id == req.params.id) ? { ...item, name, client } : item );
-  //res.json({ message: 'OK' });
-});
-
-router.delete('/products/:id', (req, res) => {
-  //const { name, client } = req.body;
-  req.db.collection('products')
-    .deleteOne({ _id: ObjectId(req.params.id) })
-    .then(() => {
-      res.json({ message: 'OK' });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: err });
-    });
-
-  //db = db.products.filter(item => item.id != req.params.id)
-  //res.json({ message: 'OK' });
-});
+router.get("/products", ProductController.getAll);
+router.get("/products/random", ProductController.getRandom);
+router.get("/products/:id", ProductController.getById);
+router.post("/products", ProductController.createProduct);
+router.put("/products/:id", ProductController.updateProduct);
+router.delete("/products/:id", ProductController.deleteProduct);
 
 module.exports = router;
