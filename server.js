19387d7bf7b9d4619f5a92e6b1968b8918572e49
Adrian Manczak <manczakadrian@gmail.com>
2024-01-07 23:20:00 +0100

    test_for_api

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-//const mongoClient = require('mongodb').MongoClient;
-
 const employeesRoutes = require('./routes/employees.routes');
 const departmentsRoutes = require('./routes/departments.routes');
 const productsRoutes = require('./routes/products.routes');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,60 +18,30 @@ app.use('/api', productsRoutes);
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
-});
-mongoose.connect('mongodb://0.0.0.0:27017/companyDB', { useNewUrlParser: true });
+})
+
+// connects our backend code with the database
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'production') dbUri = 'url to remote db';
+else if (NODE_ENV === 'test') dbUri = 'mongodb://0.0.0.0:27017/companyDBtest';
+else dbUri = 'mongodb://0.0.0.0:27017/companyDB';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
+
 db.once('open', () => {
-  console.log('Connected to the database');
+  if (NODE_ENV !== "test") {
+    console.log("Connected to the database");
+  }
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen("8000", () => {
+  if (NODE_ENV !== "test") {
+    console.log("Server is running on port: 8000");
+  }
 });
 
-
-
-
-
-
-// mongoClient.connect('mongodb://0.0.0.0:27017', { useNewUrlParser: true, useUnifiedTopology: true },
-//   (err, client) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     else {
-//       console.log('Successfully connected to the database');
-//       const db = client.db('companyDB');
-//       const app = express();
-//       app.use((req, res, next) => {
-//         req.db = db;
-//         next();
-//       });
-//       app.listen('8000', () => {
-//         console.log('Server is running on port: 8000');
-//       });
-
-
-//   db.collection('departments')
-// .insertOne({ name: 'Management' })
-// .catch((err) => {
-//   console.log(err);
-// });
-
-// db.collection('employees')
-// .findOne({ department: 'IT' })
-// .then((item) => {
-//   console.log(item);
-//  })
-//  .catch((err) => {
-//    console.log(err);
-//     //  });
-//   }
-// });
-
-
-
-
-
-
+module.exports = server;
